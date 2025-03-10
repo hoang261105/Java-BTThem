a4e7431b896e856d34aa849501ac0c630d326fb1
hoang261105 <147359595+hoang261105@users.noreply.github.com>
2025-03-10 12:02:56 +0700

    Update Bai1

--- src/Bai1.java
+++ src/Bai1.java
@@ -121,48 +121,80 @@ public class Bai1 {
                     System.out.printf("So luong so nguyen to trong mang là: %d\n", count1);
                     break;
                 case 7:
-                    // Sắp xếp số chẵn chia hết cho 3 (tăng dần)
-                    for (int i = 0; i < n - 1; i++) {
-                        for (int j = i + 1; j < n; j++) {
-                            if ((numbers[i] % 2 == 0 && numbers[i] % 3 == 0) &&
-                                    (numbers[j] % 2 == 0 && numbers[j] % 3 == 0) &&
-                                    numbers[i] > numbers[j]) {
-                                int temp = numbers[i];
-                                numbers[i] = numbers[j];
-                                numbers[j] = temp;
+                    // Đếm số lượng phần tử của từng nhóm
+                    int countEvenDiv3 = 0, countOddDiv3 = 0, countOthers = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (numbers[i] % 3 == 0 && numbers[i] % 2 == 0) {
+                            countEvenDiv3++;
+                        } else if (numbers[i] % 3 == 0 && numbers[i] % 2 != 0) {
+                            countOddDiv3++;
+                        } else {
+                            countOthers++;
+                        }
+                    }
+
+                    // Tạo các mảng để lưu từng nhóm
+                    int[] evenDiv3 = new int[countEvenDiv3];
+                    int[] oddDiv3 = new int[countOddDiv3];
+                    int[] others = new int[countOthers];
+
+                    // Phân loại số vào các mảng tương ứng
+                    int idxEven = 0, idxOdd = 0, idxOthers = 0;
+                    for (int i = 0; i < n; i++) {
+                        if (numbers[i] % 3 == 0 && numbers[i] % 2 == 0) {
+                            evenDiv3[idxEven++] = numbers[i];
+                        } else if (numbers[i] % 3 == 0 && numbers[i] % 2 != 0) {
+                            oddDiv3[idxOdd++] = numbers[i];
+                        } else {
+                            others[idxOthers++] = numbers[i];
+                        }
+                    }
+
+                    // Sắp xếp nổi bọt tăng dần cho chẵn chia hết cho 3
+                    for (int i = 0; i < countEvenDiv3 - 1; i++) {
+                        for (int j = 0; j < countEvenDiv3 - i - 1; j++) {
+                            if (evenDiv3[j] > evenDiv3[j + 1]) {
+                                int temp = evenDiv3[j];
+                                evenDiv3[j] = evenDiv3[j + 1];
+                                evenDiv3[j + 1] = temp;
                             }
                         }
                     }
 
-                    // Sắp xếp số không chia hết cho 3 (tăng dần)
-                    for (int i = 0; i < n - 1; i++) {
-                        for (int j = i + 1; j < n; j++) {
-                            if ((numbers[i] % 3 != 0) && (numbers[j] % 3 != 0) && numbers[i] > numbers[j]) {
-                                int temp = numbers[i];
-                                numbers[i] = numbers[j];
-                                numbers[j] = temp;
+                    // Sắp xếp nổi bọt tăng dần cho nhóm "các số còn lại"
+                    for (int i = 0; i < countOthers - 1; i++) {
+                        for (int j = 0; j < countOthers - i - 1; j++) {
+                            if (others[j] > others[j + 1]) {
+                                int temp = others[j];
+                                others[j] = others[j + 1];
+                                others[j + 1] = temp;
                             }
                         }
                     }
 
-                    // Sắp xếp số lẻ chia hết cho 3 (giảm dần)
-                    for (int i = 0; i < n - 1; i++) {
-                        for (int j = i + 1; j < n; j++) {
-                            if ((numbers[i] % 2 != 0 && numbers[i] % 3 == 0) &&
-                                    (numbers[j] % 2 != 0 && numbers[j] % 3 == 0) &&
-                                    numbers[i] < numbers[j]) {
-                                int temp = numbers[i];
-                                numbers[i] = numbers[j];
-                                numbers[j] = temp;
+                    // Sắp xếp nổi bọt giảm dần cho lẻ chia hết cho 3
+                    for (int i = 0; i < countOddDiv3 - 1; i++) {
+                        for (int j = 0; j < countOddDiv3 - i - 1; j++) {
+                            if (oddDiv3[j] < oddDiv3[j + 1]) {
+                                int temp = oddDiv3[j];
+                                oddDiv3[j] = oddDiv3[j + 1];
+                                oddDiv3[j + 1] = temp;
                             }
                         }
                     }
+
+                    // Gộp mảng lại theo đúng thứ tự yêu cầu
+                    int index1 = 0;
+                    for (int num : evenDiv3) numbers[index1++] = num; // Chẵn chia hết cho 3 (tăng dần)
+                    for (int num : others) numbers[index1++] = num;  // Số còn lại (tăng dần)
+                    for (int num : oddDiv3) numbers[index1++] = num; // Lẻ chia hết cho 3 (giảm dần)
 
                     // In kết quả sau khi sắp xếp
                     System.out.println("Mảng sau khi sắp xếp:");
                     for (int num : numbers) {
-                        System.out.println(num);
+                        System.out.print(num + " ");
                     }
+                    System.out.println();
                     break;
                 case 8:
                     System.out.println("Nhập 1 giá trị cần thêm: ");
